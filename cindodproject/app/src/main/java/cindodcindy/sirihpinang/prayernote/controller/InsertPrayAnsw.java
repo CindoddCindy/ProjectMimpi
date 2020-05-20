4b18e0c4d89d81a63d5956fd86cfa2c3a84597ca
CindoddCindy <cindodcindy@gmail.com>
2020-05-20 23:12:04 +0700

    baru nambah layout

---
+++ cindodproject/app/src/main/java/cindodcindy/sirihpinang/prayernote/controller/InsertPrayAnsw.java
@@ -0,0 +1,16 @@
+package cindodcindy.sirihpinang.prayernote.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cindodcindy.sirihpinang.prayernote.R;
+
+public class InsertPrayAnsw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_pray_answ);
+    }
+}
